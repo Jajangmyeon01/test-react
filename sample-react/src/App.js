a6d9b57a3778566f4caa124ce9af96e1973a790a
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-06-10 21:05:31 +0800

    Add files via upload

---
+++ sample-react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./button";
+import "./style.css";
+
+const App = () => {
+    return (
+    <div>
+        <h1 className="heading"> Microsoft! </h1>
+        <Button/>
+    </div>
+    );
+};
+export default App;
---
+++ sample-react/src/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+    return (
+        <div>
+            <a href="https://www.microsoft.com/en-ph/"><button className="btn" 
+            style={{ backgroundColor: "blue", fontSize: "22px", color: "white", width: 200, marginLeft: 820}}>Click here!</button></a>    
+        </div>
+    );
+};
+export default Button;
