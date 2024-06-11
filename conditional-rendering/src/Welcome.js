113e5de288b14a5755efaa57dea108f6746e20a5
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-06-11 09:01:07 +0800

    Add files via upload

---
+++ conditional-rendering/src/Welcome.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Welcome = ({
+    name = "Learner",
+    type = "Regular",
+    isLoggedIn = false,
+}) => {
+    if (isLoggedIn){
+        return (
+            <div>
+                <h2>Hello {name}!</h2>
+                <p>Account Type: {type}</p>
+            </div>
+        );
+    } else{
+        return (
+            <div>
+                <h2>Hello {name}!</h2>
+            </div>
+        );
+    }
+};
+export default Welcome;
